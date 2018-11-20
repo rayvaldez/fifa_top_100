ed79ba7e25bfaa6b6c1dd846dee5f53c5deda124
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-20 19:32:53 +0000

    Refactored CLI code

--- lib/fifa_top_100/cli.rb
+++ lib/fifa_top_100/cli.rb
@@ -8,10 +8,10 @@ class FifaTop100::CLI
     puts "          Welcome to the FifaTop100 CLI".colorize(:green)
     puts "----------------------------------------------------------"
     puts ""
-    top_menu
+    main_menu
   end
 
-  def top_menu
+  def main_menu
     input = nil
     puts "Please choose from the following options:"
     puts ""
@@ -19,34 +19,14 @@ class FifaTop100::CLI
     puts "2.Top 20".colorize(:yellow)
     puts "3.Top 50".colorize(:yellow)
     puts "4.Show all Players".colorize(:yellow)
-    input = gets.strip.to_i
 
-    list_players(input)
-
-    puts ""
-    puts "Select a player you would like more information on."
-    input = gets.strip.to_i
-
-    print_player(input)
-
-    puts ""
-    puts "Would you like to see another player? Enter Y or N"
-    input = gets.strip.downcase
-
-    if input == "y"
-      top_menu
-    elsif input == "n"
-      goodbye
-    else
-      puts ""
-      puts "Please type Y or N"
-      puts ""
-      top_menu
-    end
+    list_players
+    print_player
+    menu_options
   end
 
-  def list_players(input)
-    puts ""
+  def list_players
+    input = gets.strip.to_i
     if input == 1
         puts "-----------------------------------"
         puts "               Top 10".colorize(:green)
@@ -54,6 +34,7 @@ class FifaTop100::CLI
         @players[1..10].each.with_index(1) do |player, index|
         puts "#{index}. #{player.name}".colorize(:yellow)
         end
+        select_player
       elsif input == 2
         puts "-----------------------------------"
         puts "               Top 20".colorize(:green)
@@ -61,6 +42,7 @@ class FifaTop100::CLI
         @players[1..20].each.with_index(1) do |player, index|
         puts "#{index}. #{player.name}".colorize(:yellow)
         end
+        select_player
       elsif input == 3
         puts "-----------------------------------"
         puts "               Top 50".colorize(:green)
@@ -68,6 +50,7 @@ class FifaTop100::CLI
         @players[1..50].each.with_index(1) do |player, index|
         puts "#{index}. #{player.name}".colorize(:yellow)
         end
+        select_player
       elsif input == 4
         puts "-----------------------------------"
         puts "               Top 100".colorize(:green)
@@ -75,15 +58,15 @@ class FifaTop100::CLI
         @players[1..100].each.with_index(1) do |player, index|
         puts "#{index}. #{player.name}".colorize(:yellow)
         end
+        select_player
       else
-        puts ""
-        puts "Please enter a valid option".colorize(:red)
-        puts ""
-        top_menu
+        input_error
+        list_players
       end
   end
 
-  def print_player(input)
+  def print_player
+    input = gets.strip.to_i
     if input > 0 && input < 101
       the_player = @players[input.to_i]
       puts "----------------------------------------------------------------------------------------"
@@ -94,16 +77,43 @@ class FifaTop100::CLI
       puts "#{the_player.description}".colorize(:white)
       puts ""
       puts "----------------------------------------------------------------------------------------"
-    else
-      puts "Please enter a valid option".colorize(:red)
+
       puts ""
-      top_menu
+      puts "Would you like to view another player? Enter Y or N"
+      puts ""
+    else
+      input_error
+      print_player
+    end
+  end
+
+  def menu_options
+    input = gets.strip.downcase
+    if input == "y"
+      main_menu
+    elsif input == "n"
+      goodbye
+    else
+      input_error
+      menu_options
     end
   end
 
   def goodbye
     puts ""
     puts "Goodbye!"
+    puts ""
+  end
+
+  def input_error
+    puts ""
+    puts "Please enter a valid option".colorize(:red)
+    puts ""
+  end
+
+  def select_player
+    puts ""
+    puts "Select a player you would like more information on."
     puts ""
   end
 end
