7814cb11e46ed8fd3fc9d999427a313ad20179aa
rayvaldez <ray_valdez@hotmail.co.uk>
2021-01-24 18:13:07 +0000

    Update initialize method to reject nil entries

--- lib/fifa_top_100/player.rb
+++ lib/fifa_top_100/player.rb
@@ -1,12 +1,17 @@
 class FifaTop100::Player
+  attr_writer :player
   attr_accessor :name, :rank, :team, :description
   @@all = []
 
   def initialize(hash)
     hash.each do |key, value|
-      self.send("#{key}=", value)
+      if (value != "") && (value != nil)
+        self.send("#{key}=", value)
+      end
     end
-    @@all << self
+    if self.rank != nil
+      @@all << self
+    end
   end
 
   def self.all
@@ -14,6 +19,7 @@ class FifaTop100::Player
   end
 
   def self.sort_all
+    @@all.reject { |player| player.rank.nil? }
     @@all.sort! { |a, b| a.rank.to_i <=> b.rank.to_i }
   end
 
