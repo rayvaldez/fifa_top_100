df030cda4630b9af317712de010d2981880955cd
rayvaldez <ray_valdez@hotmail.co.uk>
2021-01-25 12:32:37 +0000

    Fix scraper code to show Ronaldo as No.1

--- lib/fifa_top_100/scraper.rb
+++ lib/fifa_top_100/scraper.rb
@@ -3,7 +3,7 @@ class FifaTop100::Scraper
   def self.scrape_page
     doc = Nokogiri::HTML(open("https://www.ea.com/en-gb/games/fifa/fifa-19/ratings/fifa-19-player-ratings-top-100"))
     # binding.pry
-    doc.css("ea-section-column")[3..205].each do |player|
+    doc.css("ea-section-column")[1..205].each do |player|
       FifaTop100::Player.new ({
         team: player.css("h2").text,
         name: player.css("h3").text.split(" ").drop(1).join(" "),
